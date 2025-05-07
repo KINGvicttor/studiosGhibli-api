6c6bd7607968dc1aa6ab6a75e12cd76ee09dd8d7
João Victor <victorking0820@gmail.com>
2025-05-07 19:14:29 -0300

    ajustes na borda do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return(
-        <header className="w-screen h-[150px]">
+        <header className="w-screen h-[150px] border-t-4 border-amber-300">
             <div className="container mx-auto h-[150px] flex flex-col justify-center items-center text-center">
                 <h1 className="text-5xl font-bold">Studio Ghibli Collection</h1>
                 <p className="text-md text-gray-600 mt-4">Explore the magical world of Studio Ghibli films. Mark your favorites and keep track of what <br /> you've watched.</p>
