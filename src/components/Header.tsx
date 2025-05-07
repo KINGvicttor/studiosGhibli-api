82e2ab2af8e26765c30063cfbee8d2451371cf34
João Victor <victorking0820@gmail.com>
2025-05-07 17:54:42 -0300

    header front-end concluido

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return(
+        <header className="w-screen h-[150px]">
+            <div className="container mx-auto h-[150px] flex flex-col justify-center items-center text-center">
+                <h1 className="text-5xl font-bold">Studio Ghibli Collection</h1>
+                <p className="text-md text-gray-600 mt-4">Explore the magical world of Studio Ghibli films. Mark your favorites and keep track of what <br /> you've watched.</p>
+            </div>
+        </header>
+    )
+}
