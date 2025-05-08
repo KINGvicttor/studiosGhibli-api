4b91ab637955a418540b907cc22e4f379a7d34c1
João Victor <victorking0820@gmail.com>
2025-05-08 00:01:47 -0300

    type movie criado

---
+++ src/types/Movie.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+    image: string;
+    title: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    running_time: string;
+    description: string;
+    rt_score: string;
+}
